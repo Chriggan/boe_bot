dc90629cedc900a6dd571170efa13bc222952b97
Chriggan <christian.t11111@gmail.com>
2025-11-05 17:15:18 +0100

    temp, reversing

--- temp.ino
+++ temp.ino
@@ -6,6 +6,7 @@ int servo_pin_right_wheel = 10;
 int servo_pin_left_wheel = 11;
 float prev_time = 0;
 bool crash = false;
+int reverse_until;
 
 void setup()
 {
@@ -17,10 +18,11 @@ void setup()
     pinMode(5, INPUT);
 }
 
+double a_max = 0.2;
+double a_max_emergency = 1.0;
+
 void loop()
 {
-    Serial.println(crash);
-
     float seconds_passed = micros() / 1000000.0;
     float delta_time = seconds_passed - prev_time;
 
@@ -30,23 +32,42 @@ void loop()
         return;
     }
 
+    int drive_collision = check_collision();
+
+    Serial.println(drive_collision);
+
     if (crash == false)
     {
-        drive(0.15, 0.15, delta_time);
+        drive(0.15, 0.15, delta_time, a_max);
+        if (drive_collision == 0 or drive_collision == 2)
+        {
+            drive(0, 0, delta_time, a_max_emergency);
+            crash = true;
+            reverse_until = prev_time + 3.0;
+        }
+        else if (drive_collision == 1)
+        {
+            crash = false;
+        }
     }
     else if (crash == true)
     {
-        int time_done_reverse = seconds_passed + 1.5 * 1000000;
-        if (seconds_passed < time_done_reverse)
-        {
-            reverse(0.05, 0.05, delta_time);
-            crash = false;
+        if (drive_collision == 2)
+        { // krock höger, snurrar vänster hjul snabbare
+            reverse(0.04, 0.08, delta_time, a_max);
         }
         else
+        { // vänster eller båda
+            reverse(0.08, 0.04, delta_time, a_max);
+        }
+        if (seconds_passed > reverse_until)
         {
-            Serial.println("niklas");
+            drive(0, 0, delta_time, a_max_emergency);
             crash = false;
         }
+
+        // else (){
+        // }
     }
 }
 
@@ -64,48 +85,30 @@ int left_speed_to_arduino_units(double meter_per_second)
 
 double prev_left_velocity = 0;
 double prev_right_velocity = 0;
-double a_max = 0.2;
-double a_max_emergency = 1.0;
 
-void drive(double left_speed_target, double right_speed_target, double delta_time)
+void drive(double left_speed_target, double right_speed_target, double delta_time, float acceleration)
 {
 
     double left_acc_target = (left_speed_target - prev_left_velocity) / delta_time;
-    double left_a_allowed = constrain(left_acc_target, -a_max, a_max);
+    double left_a_allowed = constrain(left_acc_target, -acceleration, acceleration);
     double left_speed_allowed = prev_left_velocity + left_a_allowed * delta_time;
     prev_left_velocity = left_speed_allowed; // in m/s
 
     double right_acc_target = (right_speed_target - prev_right_velocity) / delta_time;
-    double right_a_allowed = constrain(right_acc_target, -a_max, a_max);
+    double right_a_allowed = constrain(right_acc_target, -acceleration, acceleration);
     double right_speed_allowed = prev_right_velocity + right_a_allowed * delta_time;
     prev_right_velocity = right_speed_allowed;
 
     int r_microseconds = right_speed_to_arduino_units(right_speed_allowed);
     int l_microseconds = left_speed_to_arduino_units(left_speed_allowed);
 
-    int collision = check_collision();
-
-    if (collision == 0)
-    {
-        stop_drive();
-        crash = true;
-    }
-    else
-    {
-        motor1.writeMicroseconds(r_microseconds);
-        motor2.writeMicroseconds(l_microseconds);
-    }
+    motor1.writeMicroseconds(r_microseconds);
+    motor2.writeMicroseconds(l_microseconds);
 }
 
-void reverse(double left_speed_target, double right_speed_target, float delta_time)
+void reverse(double left_speed_target, double right_speed_target, float delta_time, float acceleration)
 {
-    drive(-left_speed_target, -right_speed_target, delta_time);
-}
-
-void stop_drive()
-{
-    motor1.writeMicroseconds(1498.2);
-    motor2.writeMicroseconds(1501.8);
+    drive(-left_speed_target, -right_speed_target, delta_time, acceleration);
 }
 
 int check_collision()
@@ -115,6 +118,9 @@ int check_collision()
 
     int sensor_values[] = {input_l, input_r};
     int temp[] = {9, 5};
+
+    Serial.println(input_r);
+    Serial.println(input_r);
 
     if (input_l == 1 and input_r == 1)
     {
@@ -130,7 +136,6 @@ int check_collision()
     }
     else if (input_r == 0)
     {
-
-        return 0; // krock höger
+        return 2; // krock höger
     }
 }
